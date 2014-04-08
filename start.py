30221977367664751459c82d884c1e230e8f69cd
Florian Ober <fober@astrophysik.uni-kiel.de>
2014-04-08 14:46:13 +0200

    modified starting script

--- start.py
+++ start.py
@@ -42,9 +42,10 @@ class worker(threading.Thread):
             
             input_parameter = para_in[1]
             set_input_file(input_parameter)
-            self.ErgebnisLock.release()
-            os.system('./mol3d > %s.dat' %(para_in[0]))
             
+            self.ErgebnisLock.release()
+            #os.system('./mol3d > %s.dat' %(para_in[0]))
+            time.sleep(2)
             self.ErgebnisLock.acquire() 
             self.Ergebnis[para_in[0]] = 'done'
             self.ErgebnisLock.release() 
@@ -59,11 +60,32 @@ def set_input_file(paralist):
         # just use the unchanged input file -> "input/input.dat"
         pass
     elif isinstance(paralist,dict):
+        
+        #~ load most recent input file
+        f = open(path_home+"input/input.dat")
+        lines = f.readlines()
+        f.close()
+        #~ save backup
+        f = open(path_home+"input/input.dat.bak",'w')
+        f.writelines(lines)
+        f.close()
+        
         # write a new input file based on the parameters given in 'paralist'
-        pass
-    pass
-
-
+        f = open(path_home+"input/input.dat", 'w')
+        for entry in lines:
+            val = entry.split()
+            if len(val) >=1:
+                if 'th_map' == val[0]:
+                    entry = 'th_map = {%.1f}                           inclination to observer\n' %(paralist['inc'])
+                if 'proname' == val[0]:
+                    entry = 'proname = {%s}                       project name\n' %(paralist['pname'])
+                if 'old_proname' == val[0]:
+                    entry = 'old_proname = {%s}                       project name\n' %(paralist['old_pname'])
+                if 'line' == val[0] :
+                    entry = 'line = {%d}                                line transition no (see gas_input file)\n' %(paralist['line'])
+            f.write(entry)
+        f.close()
+        
 def start_mol3d(vis):
     t1 = time.time()
     os.chdir(path_home)
@@ -80,18 +102,35 @@ def start_mol3d(vis):
         print(' executable succesfully created')
     else:
         sys.exit('could not find the executable, maybe Mol3d is not compiled (correctly)?')
+    # create list with parameters for the run
+    inc         = [60,45,10,0]
+    tr          = [2,7]
+    old_pname   = ['temp_mcgap','temp_mcnogap']
+    arr = [] # more general tbd
+    counter = 0
     
-    arr = ['testrun']
+    for op in range(len(old_pname)):
+        for i in range(len(inc)):
+            for t in range(len(tr)):
+                para_list = {}
+                if 'no' in old_pname[op]:
+                    para_list['pname'] = '2to9nogapi%.1f_CO%d' %(inc[i],tr[t])
+                else:
+                    para_list['pname'] = '2to9gapi%.1f_CO%d' %(inc[i],tr[t])
+                
+                para_list['old_pname'] = old_pname[op]
+                para_list['inc']   = inc[i]
+                para_list['line']  = tr[t]
+                arr.append(para_list)
+    
     my_threads = [worker(qu) for i in range(N)] 
     for thread in my_threads: 
         thread.setDaemon(True) 
         thread.start() 
 
     for g in range(len(arr)):  # put jobs into the queue
-        
-        pname = arr[g]
-        
-        parameter = [pname,'dummy']
+
+        parameter = [arr[g]['pname'],arr[g]]
         worker.ErgebnisLock.acquire() 
         worker.Ergebnis[parameter[0]] = "queued" 
         worker.ErgebnisLock.release()
@@ -99,8 +138,8 @@ def start_mol3d(vis):
         qu.put(parameter)
         # wait a bit before submitting a new job to the queue
         # This is mainly a workaround to let the code start one instance of Mol3d at this moment
-        no = random.random()*2.1
-        time.sleep(no) 
+        #~ no = random.random()*2.1
+        #~ time.sleep(no) 
   
     # some visualisation if wanted
     
