6261e34798ba4fecaebc9cb691b53076e59ae54c
Florian Ober <fober@astrophysik.uni-kiel.de>
2015-04-28 14:06:44 +0200

    new raytracer (not working yet!)

--- src/raytrace_mod.f90
+++ src/raytrace_mod.f90
@@ -32,6 +32,270 @@ MODULE raytrace_mod
     END INTERFACE
 
 CONTAINS
+FUNCTION raytrace_px_generic(basics, grid, model, dust, gas,          &
+                              coor_map1, coor_map2, i_map)                     &
+                              RESULT(px_intensity)
+
+    IMPLICIT NONE
+        !----------------------------------------------------------------------!
+        TYPE(Basic_TYP),INTENT(IN)                       :: basics
+        TYPE(Grid_TYP),INTENT(IN)                        :: grid
+        TYPE(Model_TYP),INTENT(IN)                       :: model
+        TYPE(Dust_TYP),INTENT(IN)                        :: dust
+        TYPE(Gas_TYP),INTENT(IN)                         :: gas
+        
+        !----------------------------------------------------------------------!
+        REAL(KIND=r2),INTENT(IN)                         :: coor_map1
+        REAL(KIND=r2),INTENT(IN)                         :: coor_map2
+        INTEGER,INTENT(IN)                               :: i_map
+
+        REAL(KIND=r2), DIMENSION(-gas%i_vel_chan:gas%i_vel_chan,1:gas%n_tr) :: px_intensity
+
+        REAL(KIND=r2)                                    :: ray_len
+        REAL(KIND=r2)                                    :: dz
+        REAL(KIND=r2)                                    :: dz_new
+        REAL(KIND=r2)                                    :: dz_sum
+        REAL(KIND=r1)                                    :: j_dust
+        REAL(KIND=r1)                                    :: alpha_dust
+        REAL(KIND=r1)                                    :: velo_dir_xyz
+        
+        REAL(KIND=r2)                                    :: d_l
+        REAL(KIND=r2)                                    :: cell_d_l
+        REAL(KIND=r2)                                    :: cell_sum
+        REAL(KIND=r1)                                    :: ray_minA
+        REAL(KIND=r1)                                    :: epsi
+        REAL(KIND=r2)                                    :: epsr2
+
+        REAL(KIND=r1)                                              :: gauss_val
+        REAL(KIND=r1), DIMENSION(-gas%i_vel_chan:gas%i_vel_chan)   :: intensity
+        REAL(KIND=r1)                                              :: intensity_new
+        REAL(KIND=r1)                                              :: intensity_new2
+        REAL(KIND=r1)                                              :: j_ges
+        REAL(KIND=r1)                                              :: j_ul
+        REAL(KIND=r1)                                              :: expo
+
+        REAL(KIND=r1)                                              :: alpha_ges
+        REAL(KIND=r1)                                              :: alpha_ul
+
+        REAL(KIND=r2), DIMENSION(1:3)                   :: pos_xyz
+        REAL(KIND=r2), DIMENSION(1:3)                   :: pos_xyz_new
+        REAL(KIND=r2), DIMENSION(1:3)                   :: pos_xyz_cell
+
+        REAL(KIND=r2), DIMENSION(1:6)                   :: RK_k
+
+        INTEGER                                         :: vch
+        INTEGER                                         :: tr, k
+        INTEGER                                         :: nr_cell
+        INTEGER                                         :: nr_cell_new
+
+        LOGICAL                                         :: kill_photon, log_size
+        !----------------------------------------------------------------------!
+
+        ! reset intensity in current ray
+        mode = 'LINE'
+        j_dust     = 0.0
+        j_ges      = 0.0
+        j_ul       = 0.0
+        alpha_dust = 0.0
+        alpha_ges  = 0.0
+        alpha_ul   = 0.0
+        velo_dir_xyz = 0.0
+        
+        ray_len = get_ray_length(GetGridName(grid), model%r_ou,                &
+                                 coor_map1, coor_map2)
+
+        ! model%D_2obs(1, :, i_map) gives the x-axis in the observes plane
+        ! model%D_2obs(2, :, i_map) gives the y-axis in the observes plane
+        ! model%D_2obs(3, :, i_map) gives the direction to the observer (z-axis)
+
+        pos_xyz(:) = -model%D_2obs(3, :, i_map) * sqrt(ray_len) +              &
+                      coor_map1*model%D_2obs(1, :, i_map) +                    &
+                      coor_map2*model%D_2obs(2, :, i_map)
+        px_intensity(:,:) = 0.0  
+        intensity(:)      = 0.0  
+        ray_minA          = 20.0_r2
+        epsr2             = EPSILON(dz)
+        dz                = epsr2
+        dz_sum            =  0.0_r2
+
+        log_size = .False.
+
+        IF ( ray_len .gt. 0.0_r2 ) THEN
+            nr_cell = get_cell_nr(grid,pos_xyz)
+            DO WHILE ( dz_sum*(1.0_r2+epsr2*1.0e3) .lt. 2.0_r2* sqrt(ray_len) )
+                IF ( nr_cell==0 ) THEN
+                    CALL path_skip( grid, pos_xyz,model%D_2obs(3, :, i_map), &
+                                    pos_xyz_new, nr_cell_new, d_l)
+                    dz_sum = dz_sum + d_l
+                    pos_xyz = pos_xyz_new
+                    nr_cell = nr_cell_new
+                    IF (.not. check_inside(nr_cell, grid, model) ) EXIT
+                END IF
+
+                CALL path( grid, pos_xyz, pos_xyz_new, nr_cell, nr_cell_new,   &
+                           d_l, kill_photon, model%D_2obs(3, :, i_map))
+
+                ! At this point, we have an entrance point(pos_xyz) and
+                ! exit point (pos_xyz_new) and 
+                ! the length of the cell path (d_l)
+                ! now we need to calculate the intensity depending on the mode
+                ! mode (continuum, lines)
+                SELECT CASE(mode)
+                CASE('LINE')
+                
+                CASE('CONTINUUM')
+
+                END SELECT
+                pos_xyz = pos_xyz_new
+                nr_cell = nr_cell_new
+                dz_sum = dz_sum + d_l
+            END DO !walk in z direction towards observer
+
+        px_intensity(:,1) = intensity
+        END IF
+    END FUNCTION raytrace_px_generic
+
+    FUNCTION line_intensity(grid, model, dust, gas,                          &
+                            coor_map1, coor_map2, i_map)                     &
+                            RESULT(intensity)
+    !----------------------------------------------------------------------!
+        TYPE(Grid_TYP),INTENT(IN)                        :: grid
+        TYPE(Model_TYP),INTENT(IN)                       :: model
+        TYPE(Dust_TYP),INTENT(IN)                        :: dust
+        TYPE(Gas_TYP),INTENT(IN)                         :: gas
+        
+        !----------------------------------------------------------------------!
+        REAL(KIND=r2),INTENT(IN)                         :: coor_map1
+        REAL(KIND=r2),INTENT(IN)                         :: coor_map2
+        INTEGER,INTENT(IN)                               :: i_map
+
+        REAL(KIND=r2), DIMENSION(-gas%i_vel_chan:gas%i_vel_chan,1:gas%n_tr) :: px_intensity
+
+        REAL(KIND=r2)                                    :: ray_len
+        REAL(KIND=r2)                                    :: dz
+        REAL(KIND=r2)                                    :: dz_new
+        REAL(KIND=r2)                                    :: dz_sum
+        REAL(KIND=r1)                                    :: j_dust
+        REAL(KIND=r1)                                    :: alpha_dust
+        REAL(KIND=r1)                                    :: velo_dir_xyz
+        
+        REAL(KIND=r2)                                    :: d_l
+        REAL(KIND=r2)                                    :: cell_d_l
+        REAL(KIND=r2)                                    :: cell_sum
+        REAL(KIND=r1)                                    :: ray_minA
+        REAL(KIND=r1)                                    :: epsi
+        REAL(KIND=r2)                                    :: epsr2
+
+        REAL(KIND=r1)                                              :: gauss_val
+        REAL(KIND=r1), DIMENSION(-gas%i_vel_chan:gas%i_vel_chan)   :: intensity
+        REAL(KIND=r1)                                              :: intensity_new
+        REAL(KIND=r1)                                              :: intensity_new2
+        REAL(KIND=r1)                                              :: j_ges
+        REAL(KIND=r1)                                              :: j_ul
+        REAL(KIND=r1)                                              :: expo
+
+        REAL(KIND=r1)                                              :: alpha_ges
+        REAL(KIND=r1)                                              :: alpha_ul
+
+        REAL(KIND=r2), DIMENSION(1:3)                   :: pos_xyz
+        REAL(KIND=r2), DIMENSION(1:3)                   :: pos_xyz_new
+        REAL(KIND=r2), DIMENSION(1:3)                   :: pos_xyz_cell
+
+        REAL(KIND=r2), DIMENSION(1:6)                   :: RK_k
+
+        INTEGER                                         :: vch
+        INTEGER                                         :: tr, k
+        INTEGER                                         :: nr_cell
+        INTEGER                                         :: nr_cell_new
+
+        LOGICAL                                         :: kill_photon, log_size
+        !----------------------------------------------------------------------!
+        
+        IF ( (grid%grd_mol_density(nr_cell) .gt. 1.0e-200_r2) ) THEN
+            DO tr = 1, gas%n_tr   ! more than one transition at once is
+                                  ! not yet fully implemented
+                alpha_dust =    sum(grid%grd_dust_density(nr_cell,:) * &
+                                dust%C_ext(:,dust%num_lam_map(dust%cont_map(tr))))
+
+                j_dust       =  sum(grid%grd_dust_density(nr_cell,:) * &
+                                dust%C_abs(:,dust%num_lam_map(dust%cont_map(tr))) * &
+                                planckhz(grid%t_dust(nr_cell, :),&
+                                con_c/dust%lam(dust%num_lam_map(dust%cont_map(tr)))))
+
+                j_ul =      grid%grd_mol_density(nr_cell)                     *   &
+                            grid%lvl_pop(gas%trans_upper(gas%tr_cat(tr)),nr_cell)  *   &
+                            gas%trans_einstA(gas%tr_cat(tr)) * &
+                            linescale*grid%cell_gauss_a(nr_cell)
+
+                alpha_ul =  grid%grd_mol_density(nr_cell) *            &
+                            (grid%lvl_pop(gas%trans_lower(gas%tr_cat(tr)),nr_cell) *   &
+                            gas%trans_einstB_l(gas%tr_cat(tr))                 -   &
+                            grid%lvl_pop(gas%trans_upper(gas%tr_cat(tr)),nr_cell)  *   &
+                            gas%trans_einstB_u(gas%tr_cat(tr))) *      &
+                            linescale*grid%cell_gauss_a(nr_cell)
+
+                DO vch = -gas%i_vel_chan, gas%i_vel_chan
+                    cell_d_l = d_l
+                    dz = cell_d_l * model%ref_unit
+                    cell_sum = 0.0_r2
+                    pos_xyz_cell = pos_xyz
+                    intensity_new   = 0.0
+                    intensity_new2  = 0.0
+                    DO WHILE (cell_sum .lt. d_l)
+                        RK_k(:) = 0.0
+                        DO k = 1,6
+                            IF (velo_type == 1 ) THEN
+                                ! analytical velocity distribution
+                                velo_dir_xyz =  dot_product(Set_velo(pos_xyz_cell+ &
+                                              cell_d_l*model%D_2obs(3, :, i_map) *      &
+                                              RK_c(k),model%kep_const),              &
+                                              model%D_2obs(3, :, i_map)) 
+
+                            ELSEIF (velo_type == 2 ) THEN
+                                ! linear interpolation of the velocity 
+                                velo_dir_xyz  = Get_velo(dot_product(grid%velo(nr_cell,:),model%D_2obs(3, :, i_map)), &
+                                                dot_product(grid%velo(nr_cell_new,:),model%D_2obs(3, :, i_map)), &
+                                                cell_d_l*RK_c(k)/d_l)
+                            END IF
+                            expo = -((gas%velo_channel(vch)-velo_dir_xyz)**2*      &
+                                             grid%cell_gauss_a2(nr_cell))
+                            gauss_val  = exp(expo)
+
+                            j_ges = j_ul  * gauss_val  + j_dust
+                            alpha_ges = alpha_ul * gauss_val  + alpha_dust
+
+                            RK_k(k) = ( -alpha_ges*(intensity(vch) + &
+                                        dz*dot_product(RK_a(:,k),RK_k(:)) ) + j_ges)
+                        END DO ! for all k
+
+                        intensity_new  = intensity(vch) + dz*(dot_product(RK_b1(:),RK_k(:)))
+                        intensity_new2 = intensity(vch) + dz*(dot_product(RK_b2(:),RK_k(:)))
+                        
+                        epsi= abs(intensity_new2-intensity_new)/&
+                                 ( rel_err*abs(intensity_new) + abs_err)
+                        ! dz_new = 0.9_r2*dz*exp(-log(epsi)*0.2_r2)
+                        dz_new = 0.9*dz*epsi**(-0.2)
+                        IF ( epsi .le. 1 ) THEN
+                            intensity(vch) = intensity_new
+                            pos_xyz_cell = pos_xyz_cell+cell_d_l*model%D_2obs(3, :, i_map)
+                            cell_sum = cell_sum + cell_d_l
+                            dz = MIN(dz_new,4*dz)
+                            cell_d_l = dz*model%ref_unitn
+                            IF ( cell_sum + cell_d_l .gt. d_l) THEN
+                                cell_d_l = d_l - cell_sum
+                                dz = cell_d_l*model%ref_unit
+                            END IF
+                        ELSE
+                            dz = MAX(dz_new, 0.25*dz)
+                            cell_d_l = dz*model%ref_unitn
+                        END IF
+                    END DO  ! end walk inside one cell
+                END DO !vch
+            END DO !transitions , please do only one transition per run
+        END IF
+    END FUNCTION line_intensity
+
+
     FUNCTION get_line_intensity_in_px(basics, grid, model, dust, gas,          &
                               coor_map1, coor_map2, i_map)                     &
                               RESULT(px_intensity)
